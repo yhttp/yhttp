663d0e4a9915ad0115f085394e0b5ee9d4f62bba
pylover <vahid.mardani@gmail.com>
2020-01-15 01:49:31 +0330

    v1.0.3

--- yhttp/__init__.py
+++ yhttp/__init__.py
@@ -2,5 +2,5 @@ from .application import Application
 from .validation import validate
 from .contenttypes import contenttype, binary, utf8, json, text
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
