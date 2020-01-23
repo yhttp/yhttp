181e0d4ae018c4c8c55974eec239f2ee423e8c33
pylover <vahid.mardani@gmail.com>
2020-01-24 02:21:39 +0330

    API Reference: content types

--- yhttp/contenttypes.py
+++ yhttp/contenttypes.py
@@ -4,6 +4,35 @@ import ujson
 
 
 def contenttype(contenttype=None, charset=None, dump=None):
+    """Yields a decorator to set response content type and encoding.
+
+    .. code-block::
+
+       import json
+
+       from yhttp import contenttype
+
+       @app.route()
+       @contenttype('application/json', 'utf8', json.dumps)
+       def get(req):
+           return {'foo': 'bar'}
+
+    There are ready to use contenttype decorators which can importes from
+    ``yhttp`` package, like: :func:`.json`.
+
+    .. seealso::
+
+       :func:`.binary`
+       :func:`.json`
+       :func:`.text`
+
+    :param contenttype: HTTP Content-Type, example: application/json
+    :param charset: Character set, example: utf8
+    :param dump: A ``callable(body) -> body`` to transform the body to
+                 appropriate content type.
+    :return: decorator for HTTP handlers.
+    """
+
     def decorator(handler):
         @wraps(handler)
         def wrapper(request, *a, **kw):
@@ -22,7 +51,62 @@ def contenttype(contenttype=None, charset=None, dump=None):
 
 
 binary = contenttype('application/octet-stream')
+"""
+Sets the :attr:`.Response.contenttype` to ``application/octet-stream``.
+
+Handlers must return :class:`bytes` when this decorator is used.
+
+.. code-block::
+
+   from yhttp import binary
+
+
+   @app.route()
+   @binary
+   def get(req):
+       return b'binarydata'
+
+"""
+
 utf8 = partial(contenttype, charset='utf-8')
+
+
 json = utf8('application/json', dump=ujson.dumps)
+"""
+Sets the :attr:`.Response.contenttype` to ``application/json;charset=utf-8``
+and encode the returned value by handler to json byte-string format.
+
+Handlers must return :class:`dict` when this decorator is used.
+
+.. code-block::
+
+   from yhttp import json
+
+
+   @app.route()
+   @json
+   def get(req):
+       return {'foo': 'bar'}
+
+"""
+
+
 text = utf8('text/plain')
+"""
+Sets the :attr:`.Response.contenttype` to ``text/plain;charset=utf-8``.
+
+Handlers must return :class:`str` when this decorator is used.
+
+.. code-block::
+
+   from yhttp import text
+
+
+   @app.route()
+   @text
+   def get(req):
+       return 'Unicode String'
+
+"""
+
 
